6f993b335eb1966e660c4b2d0a2f8295e33cbe6c
Clayton Hunt <ClaytonHunt_104@hotmail.com>
2012-06-21 07:46:32 -0400

    Added First Part of Classless Leveling System
    Added Game Console and a new Actor object that will be for a classless
    leveling system.

---
+++ GameEngine/GameEngine.CoreTests/ClasslessLevelingSystem/LevelingSystemTests.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using GameEngine.CoreTests.GameConsole;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GameEngine.CoreTests.ClasslessLevelingSystem
+{
+    [TestClass]
+    public class LevelingSystemTests
+    {
+        [TestMethod]
+        public void Given_PlayerEquipWeapon_When_Empty_Then_WeaponIsHands()
+        {
+            // Arrange
+            var player = new Actor();
+
+            // Act
+            player.EquipWeapon();
+
+            // Assert
+            Assert.AreEqual("Hands", player.Weapon.Name);
+        }
+
+        [TestMethod]
+        public void Given_PlayerEquipWeapon_When_Sword_Then_WeaponIsSword()
+        {
+            // Arrange
+            var player = new Actor();
+            var sword = new EquipableWeapon() { Name = "Sword" };
+            // Act
+            player.EquipWeapon(sword);
+
+            // Assert
+            Assert.AreEqual("Sword", player.Weapon.Name);
+        }
+
+        [TestMethod]
+        public void Given_PlayerAttack_When_NoTarget_Then_LogNothingToAttack()
+        {
+            // Arrange
+            var player = new Actor();
+            var console = GameConsole.GameConsole.Instance;
+
+            // Act
+            player.Attack();
+
+            // Assert
+            Assert.AreEqual("Player: There is no one to attack.", console.LastLogged());
+        }
+    }
+
+    public class Actor
+    {
+        private EquipableWeapon _weapon;
+
+        public EquipableWeapon Weapon
+        {
+            get
+            {
+                return this._weapon;
+            }
+            private set
+            {
+                this._weapon = value;
+            }
+        }
+
+        public void EquipWeapon(EquipableWeapon weapon = null)
+        {
+            if (weapon == null)
+                this.Weapon = this.DefaultWeapon();
+            else
+                this.Weapon = weapon;
+        }
+
+        protected virtual EquipableWeapon DefaultWeapon()
+        {
+            var weapon = new EquipableWeapon();
+            weapon.Name = "Hands";
+
+            return weapon;
+        }
+
+        public void Attack()
+        {
+            GameConsole.GameConsole.Instance.LogMessage("Player: There is no one to attack.");
+        }
+    }
+
+    public class EquipableWeapon
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ GameEngine/GameEngine.CoreTests/GameConsole/GameConsoleTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using CodeLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GameEngine.CoreTests.GameConsole
+{
+    [TestClass]
+    public class GameConsoleTests
+    {
+        [TestMethod]
+        public void Given_LogMessage_When_MessageProvided_Then_LogShouldContainMessage()
+        {
+            // Arrange
+            var console = GameConsole.Instance;
+
+            // Act
+            console.LogMessage("Test Message");
+
+            // Assert
+            Assert.AreEqual("Test Message", console.LastLogged());
+        }
+    }
+
+    public class GameConsole : SingletonBase<GameConsole>
+    {
+        private GameConsole()
+        {
+            _logList = new List<string>();
+        }
+
+        private List<string> _logList;
+
+        public void LogMessage(string message)
+        {
+            _logList.Add(message);
+        }
+
+        public string LastLogged()
+        {
+            return _logList.LastOrDefault();
+        }
+    }
+}
